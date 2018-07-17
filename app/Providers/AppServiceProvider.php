f41fffd3024af4d3b77964e3f354ad4b802f0e0d
Vitalii <frankovitalii@gmail.com>
2018-07-17 19:42:09 -0400

    Replace make with singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,26 +27,26 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         /*********** REPOSITORIES *********************/
-        $this->app->make(\App\Repository\Contracts\CurrencyRepository::class, \App\Repository\CurrencyRepository::class);
-        $this->app->make(\App\Repository\Contracts\LotRepository::class, \App\Repository\LotRepository::class);
-        $this->app->make(\App\Repository\Contracts\MoneyRepository::class, \App\Repository\MoneyRepository::class);
-        $this->app->make(\App\Repository\Contracts\TradeRepository::class, \App\Repository\TradeRepository::class);
-        $this->app->make(\App\Repository\Contracts\UserRepository::class, \App\Repository\UserRepository::class);
-        $this->app->make(\App\Repository\Contracts\WalletRepository::class, \App\Repository\WalletRepository::class);
+        $this->app->singleton(\App\Repository\Contracts\CurrencyRepository::class, \App\Repository\CurrencyRepository::class);
+        $this->app->singleton(\App\Repository\Contracts\LotRepository::class, \App\Repository\LotRepository::class);
+        $this->app->singleton(\App\Repository\Contracts\MoneyRepository::class, \App\Repository\MoneyRepository::class);
+        $this->app->singleton(\App\Repository\Contracts\TradeRepository::class, \App\Repository\TradeRepository::class);
+        $this->app->singleton(\App\Repository\Contracts\UserRepository::class, \App\Repository\UserRepository::class);
+        $this->app->singleton(\App\Repository\Contracts\WalletRepository::class, \App\Repository\WalletRepository::class);
 
         /*********** REQUESTS *********************/
-        $this->app->make(\App\Request\Contracts\AddCurrencyRequest::class, \App\Request\AddCurrencyRequest::class);
-        $this->app->make(\App\Request\Contracts\AddLotRequest::class, \App\Request\AddLotRequest::class);
-        $this->app->make(\App\Request\Contracts\BuyLotRequest::class, \App\Request\BuyLotRequest::class);
-        $this->app->make(\App\Request\Contracts\CreateWalletRequest::class, \App\Request\CreateWalletRequest::class);
-        $this->app->make(\App\Request\Contracts\MoneyRequest::class, \App\Request\MoneyRequest::class);
+        $this->app->singleton(\App\Request\Contracts\AddCurrencyRequest::class, \App\Request\AddCurrencyRequest::class);
+        $this->app->singleton(\App\Request\Contracts\AddLotRequest::class, \App\Request\AddLotRequest::class);
+        $this->app->singleton(\App\Request\Contracts\BuyLotRequest::class, \App\Request\BuyLotRequest::class);
+        $this->app->singleton(\App\Request\Contracts\CreateWalletRequest::class, \App\Request\CreateWalletRequest::class);
+        $this->app->singleton(\App\Request\Contracts\MoneyRequest::class, \App\Request\MoneyRequest::class);
 
         /*********** RESPONSES *********************/
-        $this->app->make(\App\Response\Contracts\LotResponse::class, \App\Response\LotResponse::class);
+        $this->app->singleton(\App\Response\Contracts\LotResponse::class, \App\Response\LotResponse::class);
 
         /*********** SERVICES *********************/
-        $this->app->make(\App\Service\Contracts\CurrencyService::class, \App\Service\CurrencyService::class);
-        $this->app->make(\App\Service\Contracts\MarketService::class, \App\Service\MarketService::class);
-        $this->app->make(\App\Service\Contracts\WalletService::class, \App\Service\WalletService::class);
+        $this->app->singleton(\App\Service\Contracts\CurrencyService::class, \App\Service\CurrencyService::class);
+        $this->app->singleton(\App\Service\Contracts\MarketService::class, \App\Service\MarketService::class);
+        $this->app->singleton(\App\Service\Contracts\WalletService::class, \App\Service\WalletService::class);
     }
 }
